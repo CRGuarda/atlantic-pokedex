21b5596118f7950576ac468576fe332b6662dd90
Carlos Guarda <crguarda95@gmail.com>
2024-06-06 11:55:16 -0500

    fix hidden abilities

--- src/components/pokemon/pokemonDetails/PokemonDetailsAbilites.tsx
+++ src/components/pokemon/pokemonDetails/PokemonDetailsAbilites.tsx
@@ -5,13 +5,15 @@ export const PokemonDetailsAbilities = ({ label, abilitiesList }: { label: strin
     <div className='flex justify-center flex-col items-center'>
       <span className='font-bold uppercase text-xs'>{label}</span>
       <div className='flex gap-2 flex-wrap justify-center'>
-        {abilitiesList.map(({ slot, ability }) => {
-          return (
-            <span key={slot} className='bg-zinc-200 rounded-lg px-1 uppercase'>
-              {ability.name}
-            </span>
-          )
-        })}
+        {abilitiesList
+          .filter(({ is_hidden }) => !is_hidden)
+          .map(({ slot, ability }) => {
+            return (
+              <span key={slot} className='bg-zinc-200 rounded-lg px-1 uppercase'>
+                {ability.name}
+              </span>
+            )
+          })}
       </div>
     </div>
   )
